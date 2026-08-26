3e39c60e58d61935aab49f8866642e1ed2d7f5dc
TAMTARA <karenpbq@gmail.com>
2026-08-26 09:06:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'consultorio-psicologico-secret-key-2026'
     
-    # La variable DATABASE_URL la proporcionará Render con las credenciales de Supabase
+    # Intenta leer la URL de Render/Supabase
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
-    # Fallback a SQLite local por si pruebas el código en tu computadora sin conexión
+    # Fallback por seguridad
     if not SQLALCHEMY_DATABASE_URI:
         SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database', 'consultorio.db')
         
